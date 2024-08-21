4529335daee57e2e8c660da934383de47ab4c894
extern-joerg <extern.joerg.beicht@cariad.technology>
2024-08-21 10:52:00 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,4 +13,29 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBonkypwWDw96NNJBbC9p2omP13NPKJAZg",
+//   authDomain: "cm-stripe-demo-cbe08.firebaseapp.com",
+//   projectId: "cm-stripe-demo-cbe08",
+//   storageBucket: "cm-stripe-demo-cbe08.appspot.com",
+//   messagingSenderId: "544113865877",
+//   appId: "1:544113865877:web:524bae0258bb5f9ec1979c",
+//   measurementId: "G-0DJVDD8M7Z"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+}
